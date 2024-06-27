98284620304be20e7e55b055abdc537dab452e19
Renato Filho <renatosousafilho@gmail.com>
2024-06-27 17:33:49 -0300

    fix: ajusta sequelize para funcionar em produção

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PassengersModule } from './passengers/passengers.module';
 import { SequelizeModule } from '@nestjs/sequelize';
-import Passenger from './passengers/passenger.model';
+import pg from 'pg';
 
 @Module({
   imports: [
     PassengersModule,
     SequelizeModule.forRoot({
-      dialect: 'sqlite',
-      storage: ':memory:',
-      autoLoadModels: true,
-      synchronize: true,
-      models: [Passenger],
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      dialectModule: pg,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
     }),
   ],
   controllers: [AppController],
